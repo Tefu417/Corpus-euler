e2a0cabfe22722990cb48a27afa62c9f659017df
Emi <sakura.math.sashimo@gmail.com>
2020-06-20 17:39:58 +0900

    make corpus

--- create-corpus.py
+++ create-corpus.py
@@ -1,19 +1,10 @@
-import re
-import sys
-# 入力するファイル名をコマンドライン引数から受け取る
-# ターミナルで実行
-# python3 create-corpus.py corpus-euler/euler02.txt
-import pathlib
-import os
+import glob
 
-f = sys.argv[1]
-p_f = pathlib.Path(f)
+with open("corpus-euler.csv", "a") as newfile :
 
-with open(p_f, "r") as oldfile, \
-        open("corpus-euler/c_euler" + str(f[15:17]) + ".txt", "w") as newfile :
-
-    F = oldfile.read()
-    F = F.replace(" # ", ",")
-    F = F.replace("\t", "")
-
-    newfile.write(F)
+    for f in glob.glob("csv-euler/*.csv") :
+        F = open(f, "r")
+        F = F.read()
+        F = F.replace("\n\n", "")
+        
+        print(F, file = newfile)
---
+++ create-csv.py
@@ -0,0 +1,19 @@
+import re
+import sys
+# 入力するファイル名をコマンドライン引数から受け取る
+# ターミナルで実行
+# python3 create-corpus.py corpus-euler/euler02.txt
+import pathlib
+import os
+
+f = sys.argv[1]
+p_f = pathlib.Path(f)
+
+with open(p_f, "r") as oldfile, \
+        open("csv-euler/c_euler" + str(f[15:17]) + ".csv", "w") as newfile :
+
+    F = oldfile.read()
+    F = F.replace(" # ", ",")
+    F = F.replace("\t", "")
+
+    newfile.write(F)
