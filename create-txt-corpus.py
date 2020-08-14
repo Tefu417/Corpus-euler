199b250cf5de84d5589a5db39dc524785f4dad65
Emi <sakura.math.sashimo@gmail.com>
2020-08-14 13:36:20 +0900

    create corpus

--- create-txt-corpus.py
+++ create-txt-corpus.py
@@ -8,6 +8,7 @@ with open('corpus-euler.txt', 'a') as newfile :
                 line = line.replace('\t', '')
                 n = line.find(' # ')
                 c = line[: n]
-                ja = line[n + 3 :]
-                l = str(c + '\t' + ja)
+                ja = line[n + 3 : -1]
+                l = str('<SOS>' + c + '<tab>' + ja + '<EOS>')
                 newfile.write(l)
+                newfile.write('\n')
